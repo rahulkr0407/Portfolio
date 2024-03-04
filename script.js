50de242c467008b8606910ea99e8486037675e7e
Rahul <93023973+rahulkr0407@users.noreply.github.com>
2024-03-04 11:52:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".multiple-text",{
+    strings:["Student","Frontend Developer","Web Designer","Data Analyst"],
+    typeSpeed:150,
+    backSpeed:150,
+    backDelay:1000,
+    loop:true
+})
